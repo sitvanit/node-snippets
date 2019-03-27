23acd434a089b917d1f5ba8b1b8c5c78c8123ec5
t_melts <sitvanit.meltzer@autodesk.com>
2019-03-27 15:00:31 +0200

    remove comments

--- streams/2readableStream.js
+++ streams/2readableStream.js
@@ -27,7 +27,7 @@ const readableFeed = () => {
 const feed = readableFeed();
 
 feed.on('readable', () => {
-    let data = feed.read(); // pulls some data out of the internal buffer and returns it. If no data available to be read, null is returned.
+    let data = feed.read();
 
     if (data) {
         console.log(JSON.stringify(data));
