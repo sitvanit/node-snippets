11a45396916695392de80b4881ddeacf6944ef85
t_melts <sitvanit.meltzer@autodesk.com>
2019-04-27 23:06:29 +0300

    Fix

--- streams/2readableStream.js
+++ streams/2readableStream.js
@@ -1,7 +1,7 @@
 const MyReadable = require('./1MyReadable');
 
 const data = [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }, { e: 5 }];
-const readable = new MyReadable(data, { objectMode:true, highWaterMark: 2 });
+const readable = new MyReadable(data, { objectMode: true, highWaterMark: 2 });
 
 /** Consuming Readable Stream **/
 readable.on('readable', () => {
