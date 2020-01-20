b57915b60e045ce24a32fd4da0aa54c79837400d
meltzes <sitvanit.meltzer@autodesk.com>
2020-01-20 14:13:40 +0200

    fix

--- conference/allSettled.js
+++ conference/allSettled.js
@@ -1,8 +1,8 @@
 const promise1 = Promise.resolve(1);
 const promise2 = new Promise((resolve, reject) => setTimeout(reject, 100, 2));
 
-Promise.all([promise1, promise2])
-    .then(console.log);
+// Promise.all([promise1, promise2])
+//     .then(console.log);
 
 Promise.allSettled([promise1, promise2])
     .then(console.log);
